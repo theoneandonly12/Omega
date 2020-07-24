6d52de72612f54e4b99824379d7f146090bbb7b5
theoneandonly12 <68725530+theoneandonly12@users.noreply.github.com>
2020-07-23 23:31:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAVAodBCdOSmfIJo80LoIXmm2h402Xo8es",
+  authDomain: "hacking-710fd.firebaseapp.com",
+  databaseURL: "https://hacking-710fd.firebaseio.com",
+  projectId: "hacking-710fd",
+  storageBucket: "hacking-710fd.appspot.com",
+  messagingSenderId: "171235422539",
+  appId: "1:171235422539:web:eb957a834907ea524378ce",
+  measurementId: "G-JRFHRX00L7"
 };
 
      
